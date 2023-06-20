f27ee1b906179da3c5713ace764b6dd5a5da9f7c
kelvinyelyen <kelvinyelyen@gmail.com>
2023-06-20 20:12:45 +0000

    File configuration

--- project/app.py
+++ project/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 
+#configure application 
 app = Flask(__name__)
-app.debug = True
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+#login
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 if __name__ == '__main__':
